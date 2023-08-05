99c0304e60751a4530f5a961168e458e5483724f
0613jyw@naver.com <101396034+yewonjune@users.noreply.github.com>
2023-08-05 20:26:21 +0900

    EmenyBHpBar
    HpBar 생성

--- Enemy.cs
+++ Enemy.cs
@@ -13,6 +13,13 @@ public class Enemy : MonoBehaviour
     public float Speed; // 속도
     public int nextMove;
 
+    public GameObject prfHpBar;
+    public GameObject canvas;
+
+    RectTransform hpBar;
+
+    public float height = 0.8f;
+
     public bool isDead = false; //사망 여부
     void Awake()
     {
@@ -24,19 +31,19 @@ public class Enemy : MonoBehaviour
     }
 
     void FixedUpdate()
-     {
-         //움직임
-         rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+    {
+        //움직임
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
 
-         //발판 확인
-         Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.2f, rigid.position.y);
-         Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
-         RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("platform"));
-         if (rayHit.collider == null)
-         {
+        //발판 확인
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove * 0.2f, rigid.position.y);
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("platform"));
+        if (rayHit.collider == null)
+        {
             Turn();
         }
-     }
+    }
 
     void Think()
     {
@@ -47,7 +54,7 @@ public class Enemy : MonoBehaviour
         anim.SetInteger("WalkSpeed", nextMove);
 
         // Filp Sprite
-        if(nextMove != 0)
+        if (nextMove != 0)
             spriteRenderer.flipX = nextMove == 1;
 
         //Recursive
@@ -57,6 +64,7 @@ public class Enemy : MonoBehaviour
 
     void Turn()
     {
+        // 문워크 방지
         nextMove *= -1;
         spriteRenderer.flipX = nextMove == 1;
 
@@ -71,20 +79,25 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {
-
+        // 체력바 생성
+        hpBar = Instantiate(prfHpBar, canvas.transform).GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // 체력바 월드 좌표를 UI 좌표로 바꿔주는 함수
+        Vector3 _hpBarPos =
+            Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
 
+        //스크린 좌표로 바꾼 값으로 체력바 이동
+        hpBar.position = _hpBarPos;
     }
 
-    void EnemyDie ()
+    void EnemyDie()
     {
 
 
     }
-   
+
 
 }
